6a59c42aac694fafe32099a793f3b70dd21f5f7b
Görkem OCAK <grkmock@gmail.com>
2026-01-14 16:47:44 +0300

    V2

--- alembic/env.py
+++ alembic/env.py
@@ -20,19 +20,40 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline():
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    url = os.getenv("DATABASE_URL")
+    if not url:
+        raise RuntimeError("DATABASE_URL environment variable is not set")
+
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+    )
 
     with context.begin_transaction():
         context.run_migrations()
 
 
+
 def run_migrations_online():
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        raise RuntimeError("DATABASE_URL environment variable is not set")
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        {
+            "sqlalchemy.url": database_url
+        },
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
